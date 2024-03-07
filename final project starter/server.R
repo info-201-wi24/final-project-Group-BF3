1fc3286ad82d11302631c02e795598d899f4b5ae
Sarah-Xyw <159319206+Sarah-Xyw@users.noreply.github.com>
2024-03-06 17:24:50 -0800

    Update server.R

--- final project starter/server.R
+++ final project starter/server.R
@@ -36,42 +36,36 @@ server <- function(input, output) {
              yaxis = list(title = "Total Vaccinations", showticklabels = FALSE),
              hovermode = "closest")
   })
+
+
+
+
   
-  output$continentPlot <- renderPlotly({
-  filtered_data<- joined_data %>%
-  group_by(continent) %>%
-  summarise(average_gdp_per_year = mean(gdp_per_year, na.rm = TRUE),
-  average_gdp_growth_rate = mean(average_gdp_per_year/year, na.rm = TRUE),
-  vaccination_rate = mean(people_vaccinated_per_hundred, na.rm = TRUE))
-  y_data <- if(input$dataChoice == "average_gdp_growth_rate") 
-  filtered_data$average_gdp_growth_rate else filtered_data$vaccination_rate 
-  y_label <- if(input$dataChoice == "average_gdp_growth_rate") "Average GDP Growth Rate" else "Vaccination Rate (%)" 
-  plot_ly(filtered_data, x = ~average_gdp_per_year, y = ~y_data, 
-  type = 'scatter',
-  mode = 'markers',
-  text = ~continent,
-  marker = list(size = 15)) %>% 
-  layout(title = paste("GDP Growth Rate vs. Vaccine Rollout Speed", input$dataChoice),
-  xaxis = list(title = "Average GDP per Year"), 
-  yaxis = list(title = y_label)) 
+joined_data <- joined_data
+server <- function(input, output){
+  
+  
+  output$Plot3 <- renderPlotly({
+    print(class(joined_data))
+    filteredData <- joined_data %>%
+      dplyr:: filter(year == as.numeric(input$dataChoice))
+    
+    my_plot <- ggplotly(filteredData,
+                        x = GDP_USD,
+                        y = people_vaccinated_per_hundred,
+                        type = "scatter",
+                        mode = "markers",
+                        marker = list(size = 9,
+                                      color = GDP_USD,
+                                      colorscale = "red"),              
+
+  
+)
+
+  
   })
-  output$continentPlot <- renderPlotly({
-  data_to_plot <- joined_data %>% group_by(continent) %>% 
-    summarise(average_gdp_per_year = mean(gdp_per_year, na.rm = TRUE), 
-    average_total_cases = mean(total_cases_per_million, na.rm = TRUE), 
-    Vaccine_rollout_speed = mean(people_vaccinated_per_hundred, na.rm = TRUE)) 
-  y_data <- if(input$dataChoice == "average_total_cases") 
-  data_to_plot$average_total_cases else data_to_plot$Vaccine_rollout_speed 
-  y_label <- if(input$dataChoice == "average_total_cases") "Average Total Cases per Million" else "Vaccination Rate (%)" 
-  plot_ly(data_to_plot, 
-  x = ~average_gdp_per_year, 
-  y = ~y_data, type = 'scatter', 
-  mode = 'markers', 
-  text = ~continent, marker = list(size = 15))%>% 
-  layout(title = "GDP Growth Rate vs. Vaccine Rollout Speed", 
-  xaxis = list(title = "Average GDP per year”), 
-  yaxis = list(title = y_label)
-  )
-  })
+  
+  
+}
 
 }  
